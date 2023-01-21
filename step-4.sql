30f916e916dcbb3d017ea75149f8f8639011b2bb
Christopher Van Horn <christopherjvanhorn3@gmail.com>
2023-01-20 19:01:19 -0800

    finished module 2 paired programming 1 hooray

--- step-4.sql
+++ step-4.sql
@@ -1,15 +1,29 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
 -- (expected: 7 rows, starting with "Blackwoods")
-
+SELECT p.name AS park_name, c.name AS campground_name, c.open_from_mm, c.open_to_mm, c.daily_fee
+FROM park p
+JOIN campground c USING(park_id)
+ORDER BY p.name, c.name;
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
 -- (expected: 3 rows, starting with "Acadia")
-
+SELECT p.name AS park_name, COUNT(c.campground_id) AS num_of_campgrounds
+FROM park p
+JOIN campground c USING(park_id)
+GROUP BY park_id
+ORDER BY park_name;
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
 -- (expected: 12 rows)
-
+SELECT p.name AS park_name, c.name AS campground_name, s.site_id, s.max_occupancy, s.accessible, s.max_rv_length, s.utilities
+FROM park p
+JOIN campground c USING(park_id)
+JOIN site s USING(campground_id)
+WHERE c.name = 'Blackwoods';
 
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
 -- (expected: 44 rows, starting with the earliest date)
-
+SELECT s.site_number, r.name AS reservation_name, r.from_date, r.to_date
+FROM site s
+JOIN reservation r USING(site_id)
+ORDER BY r.from_date;
--- step-5.sql
+++ step-5.sql
@@ -9,13 +9,17 @@
   description: Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.
   ------------------------------
 */
-
+--BEGIN;
+INSERT INTO park(park_id, name, location, establish_date, area, visitors, description)
+VALUES (DEFAULT, 'Ohiopyle State Park', 'Pennsylvania', '1965-01-01', 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.');
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
 
 */
-
+UPDATE park
+SET visitors = 1500000
+WHERE name = 'Ohiopyle State Park';
 
 /*
  STEP THREE: Insert new campground with the following data:
@@ -27,7 +31,8 @@
   daily_fee: 95.00
   ------------------------------------------------------------
 */
-
+INSERT INTO campground(campground_id, park_id, name, open_from_mm, open_to_mm, daily_fee)
+VALUES (DEFAULT, (SELECT park_id FROM park WHERE name = 'Ohiopyle State Park'), 'Youghiogheny', 01, 12, 95.00);
 
 /*
  STEP FOUR: Insert 3 new sites with the following data:
@@ -37,7 +42,11 @@
   site_number: 625, campground_id: (use the id of the new campground you just added)
  ------------------------------------------------------------
 */
-
+INSERT INTO site(site_id, campground_id, site_number)
+VALUES 
+(DEFAULT, (SELECT campground_id FROM campground WHERE name = 'Youghiogheny'), 623), 
+(DEFAULT, (SELECT campground_id FROM campground WHERE name = 'Youghiogheny'), 624), 
+(DEFAULT, (SELECT campground_id FROM campground WHERE name = 'Youghiogheny'), 625);
 
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
@@ -47,16 +56,25 @@
   site_id: (the site_id for site number 625 that you just created), name: Kent Family, from_date: today + 12 days, to_date: today + 20 days
 ------------------------------------------------------------------------------------
 */
-
+INSERT INTO reservation(reservation_id, site_id, name, from_date, to_date)
+VALUES 
+(DEFAULT, (SELECT site_id FROM site WHERE site_number = 623), 'Wayne Family', CURRENT_DATE + 10, CURRENT_DATE + 20), 
+(DEFAULT, (SELECT site_id FROM site WHERE site_number = 624), 'Parker Family', CURRENT_DATE + 11, CURRENT_DATE + 20), 
+(DEFAULT, (SELECT site_id FROM site WHERE site_number = 625), 'Kent Family', CURRENT_DATE + 12, CURRENT_DATE + 20);
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
+UPDATE reservation
+SET from_date = CURRENT_DATE, to_date = CURRENT_DATE + 7
+WHERE name = 'Wayne Family';
 
 
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
 */
+DELETE FROM reservation
+WHERE name = 'Kent Family';
 
